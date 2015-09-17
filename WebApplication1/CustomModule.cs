7c3e2351a86554d7a7732af4920b8ecdb550a3b3
dishiyicijinqiu <458141536@qq.com>
2015-09-17 08:16:26 +0800

    3333333

---
+++ WebApplication1/CustomModule.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class CustomModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(context_BeginRequest);
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication ap = sender as HttpApplication;
+            if (ap != null)
+            {
+                ap.Response.Write("汤姆大叔测试PreApplicationStartMethod通过！<br/>");
+            }
+        }
+
+        public void Dispose()
+        {
+            //nothing to do here
+        }
+    }
+    public class PreApplicationStartCode
+    {
+        private static bool hasLoaded;
+
+        public static void PreStart()
+        {
+            if (!hasLoaded)
+            {
+                hasLoaded = true;
+                //注意这里的动态注册，此静态方法在Microsoft.Web.Infrastructure.DynamicModuleHelper
+                DynamicModuleUtility.RegisterModule(typeof(CustomModule));
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript" src="Scripts/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript">
+        function testfun()
+        {
+            alert("OK");
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input type="button" value="测试" onclick="testfun();" />
+        </div>
+    </form>
+</body>
+</html>
